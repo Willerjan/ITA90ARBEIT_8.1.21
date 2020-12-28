849d581ed457de883600940c34abfb446a7e856c
forceplayeferjan <45130501+forceplayeferjan@users.noreply.github.com>
2020-12-28 15:20:01 +0100

    Projektdateien hinzufügen.

---
+++ ITA90ARBEIT_8.1.21/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ITA90ARBEIT_8._1._21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperatur";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(635, 403);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(8, 4);
+            this.listBox1.TabIndex = 1;
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Items.AddRange(new object[] {
+            "Kelvin",
+            "Celsius",
+            "Fahrenheit"});
+            this.cmb1.Location = new System.Drawing.Point(297, 91);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(137, 24);
+            this.cmb1.TabIndex = 2;
+            this.cmb1.Text = "Kelvin";
+            this.cmb1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 22);
+            this.textBox1.TabIndex = 4;
+           // this.textBox1.DockChanged += new System.EventHandler(this.textBox1_DockChanged);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Items.AddRange(new object[] {
+            "Kelvin",
+            "Celsius",
+            "Fahrenheit"});
+            this.cmb2.Location = new System.Drawing.Point(297, 200);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(137, 24);
+            this.cmb2.TabIndex = 5;
+            this.cmb2.Text = "Kelvin";
+            this.cmb2.TextChanged += new System.EventHandler(this.cmb2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Temperatur";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            //this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 307);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ITA90ARBEIT_8.1.21/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ITA90ARBEIT_8._1._21
+{
+    public partial class Form1 : Form
+    {
+
+
+        public double zahl1, zahl2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+        }
+         
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kontrolle();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kontrolle();
+        }
+
+     
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            kontrolle();
+        }
+
+        private void cmb2_TextChanged(object sender, EventArgs e)
+        {
+            kontrolle();
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            kontrolle();
+        }
+
+
+
+        public void kontrolle()
+        {
+
+            if (textBox1.Text == "")
+            {
+
+            }
+            else
+            {
+                zahl1 = double.Parse(textBox1.Text);
+
+                Update();
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char überbrüffe = e.KeyChar;
+            if(!Char.IsDigit(überbrüffe) && überbrüffe != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Bitte nur Zahlen eingeben");
+            }
+        }
+
+       
+        public void Update()
+        {
+            if(cmb1.Text == "Kelvin")
+            {
+                if(cmb2.Text == "Kelvin")
+                {
+                    zahl2 = zahl1;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if(cmb2.Text == "Celsius")
+                {
+                    zahl2 = zahl1 - 273.15;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if(cmb2.Text == "Fahrenheit")
+                {
+                    zahl2 = zahl1 * 1.8 - 459.67;
+                    textBox2.Text = zahl2.ToString();
+                }
+            }
+            if (cmb1.Text == "Celsius")
+            {
+                if(cmb2.Text == "Kelvin")
+                {
+                    zahl2 = zahl1 + 273.15;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if(cmb2.Text == "Celsius")
+                {
+                    zahl2 = zahl1;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if(cmb2.Text == "Fahrenheit")
+                {
+                    zahl2 = zahl1 * 1.8 + 32;
+                    textBox2.Text = zahl2.ToString();
+                }
+            }
+            if (cmb1.Text == "Fahrenheit")
+            {
+                if (cmb2.Text == "Kelvin")
+                {
+                    zahl1 = zahl1 + 459.67;
+                    zahl2 = zahl1 * 5 / 9;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if (cmb2.Text == "Celsius")
+                {
+                    zahl1 = zahl1 - 32;
+                    zahl2 = zahl1 * 5 / 9;
+                    textBox2.Text = zahl2.ToString();
+                }
+                if (cmb2.Text == "Fahrenheit")
+                {
+                    zahl2 = zahl1;
+                    textBox2.Text = zahl2.ToString();
+                }
+            }
+        }
+    }
+}
